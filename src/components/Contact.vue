ee118f2e70bf49b8ffc7a8be9a6213aead4a77b7
maanderz <mandyfung93@gmail.com>
2020-11-05 18:42:28 -0700

    changed contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,31 +1,18 @@
 <template>
-    <section class="page-section" id="contact">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-8 text-center">
-                    <h2 class="mt-0">Contact</h2>
-                    <hr class="divider my-4" />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-4 mr-auto text-center">
-                    <a class="link" href="https://www.linkedin.com/in/arensmandyk/">LinkedIn</a>
-                </div>
-                <div class="col-lg-4 mr-auto text-center">
-                    <a class="link" href="https://github.com/maanderz/">Github</a>
-                </div>
-            </div>
-            <hr class="divider my-4" />
-            <div style="text-align: center;"> Located in Calgary, Alberta </div>
-        </div>
+    <section class="page-section">
+        <h2> Contact </h2>
+        <div> arens.mandyk@gmail.com </div>
     </section>
 </template>
 
 <style scoped>
     .page-section {
+        margin-top: 1rem;
+        font-family: 'EB Garamond', serif;
         color: white;
         background-color: #F9A602;
         padding: 2rem;
+        text-align: center;
     }
 
     .link {
