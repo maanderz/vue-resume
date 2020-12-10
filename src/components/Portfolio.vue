89ae86e48e234300bf1dc37cc01c66231b56ac85
maanderz <mandyfung93@gmail.com>
2020-12-10 09:43:37 -0700

    changed ordering of portfolio

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,5 +1,18 @@
 <template>
     <div id="portfolio">
+        
+        <div class="feintuch-2">
+            <div class="images">
+                <img src="../assets/maps-desktop.png" alt="desktop-1" class="img">
+                <img src="../assets/maps-mobile.png" alt="desktop-1" class="img-3">
+            </div>
+            <div class="content-2"> 
+                <h3> MapsMapsMaps  </h3>
+                <p> Vue, Express, JavaScript, Jest </p>
+                <p> A Vue app build with Google JavaScript API. Users are able to search and save addresses. Built with Vue and ran unit tests with Jest. </p>
+                <button class="btn btn-warning button"> <a class="link" href="https://sleepy-chamber-28831.herokuapp.com/">View Site </a></button>
+            </div>
+        </div>
         <div class="corinthian">
             <div class="images">
                 <img src="../assets/desktop.png" alt="desktop-1" class="img">
@@ -7,7 +20,7 @@
             </div>
             <div class="content"> 
                 <h3> Corinthian Distributors </h3>
-                <p> Wordpress, SASS, jQuery </p>
+                <p> PHP, Sass, Javascript </p>
                 <p> Corinthian Distributors specializes in importing and distributing the most popular Filipino and Asian foods to Canadian grocery stores and customers. </p>
                 <button class="btn btn-warning button"><a class="link" href="https://corinthiandistributors.com/"> View Site </a></button>
             </div>
@@ -20,7 +33,7 @@
             </div>
             <div class="content-2"> 
                 <h3> Feintuch Law  </h3>
-                <p> Wordpress, SASS, jQuery </p>
+                <p> PHP, Sass, Javascript </p>
                 <p> Feintuch Law is a boutique real estate law firm offering sophisticated legal representation, personal attention, and private real estate financing. </p>
                 <button class="btn btn-warning button"> <a class="link" href="https://feintuchlaw.com/">View Site </a></button>
             </div>
@@ -33,22 +46,9 @@
             </div>
             <div class="content"> 
                 <h3> Save The Sales </h3>
-                <p> Wordpress, SASS, jQuery </p>
+                <p> PHP, Sass, Javascript </p>
                 <p> A campaign in Victoria, BC to encourage people to shop local and be entered to win prizes.  The design look and feel were inspired by the 1980s - so retro! </p>
                 <button class="btn btn-warning button"><a class="link" href="https://savethesales.ca/"> View Site </a></button>
-            </div>
-        </div>
-
-        <div class="feintuch-2">
-            <div class="images">
-                <img src="../assets/maps-desktop.png" alt="desktop-1" class="img">
-                <img src="../assets/maps-mobile.png" alt="desktop-1" class="img-3">
-            </div>
-            <div class="content-2"> 
-                <h3> MapsMapsMaps  </h3>
-                <p> Vue, Express, JavaScript, Jest </p>
-                <p> A Vue app build with Google JavaScript API. Users are able to search and save addresses. Built with Vue and ran unit tests with Jest. </p>
-                <button class="btn btn-warning button"> <a class="link" href="https://sleepy-chamber-28831.herokuapp.com/">View Site </a></button>
             </div>
         </div>
     </div>
